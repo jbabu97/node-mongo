43d49b3665b4dbacc8f84138f5ff2a57ce17aefe
Jabbar Babu <jabbar.babu81@gmail.com>
2021-03-24 01:59:01 +0600

    my first nose project

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`Today I'm start learning Node.js`)
+})
+
+app.listen(3000, () => console.log('Listening to port 3000'))
